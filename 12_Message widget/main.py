b7459c7280ea009f44755b3e30bccc14ccd4eef8
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-09-05 18:02:46 +0900

    Delete all file traning for New Learning

--- 12_Message widget/main.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-master = Tk()
-
-msg = Message(master, text="this is a long message!", width=130)
-msg.pack()
-
-master.mainloop()
--- 18_Application Windows/statusbar.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-
-root = Tk()
-
-l = Label(root, text=" ", relief=SUNKEN, bd=3, anchor=W)
-l.pack(side=BOTTOM, fill=X)
-
-root.mainloop()
