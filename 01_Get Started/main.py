0cf1dd210353344ad89ece77cb90ef10697e804c
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-09-29 22:48:14 +0900

    Learn how to make First Window in tkinter

---
+++ 01_Get Started/main.py
@@ -0,0 +1,22 @@
+"""
+authors : Juliao Martins
+
+you want to used the tkinter module, needed the importing module, then:
+
+import tkinter
+import tkinter as tk
+	or
+from tkinter import *
+
+depending your code!
+
+"""
+
+from tkinter import *
+
+root = Tk()
+root.title("Fist Window") # this method to change the window tilte
+root.geometry("400x200") # to change the size of window
+
+
+root.mainloop()
