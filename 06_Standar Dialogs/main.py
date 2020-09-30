11fe2fe2f500d26efd2e598c84dd97c87d7fc2d0
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-01 00:46:13 +0900

    Learn message boxes in tkmessagebox with total seven displaying dialogs

--- 06_Standar Dialogs/main.py
+++ 06_Standar Dialogs/main.py
@@ -6,6 +6,26 @@ root.iconbitmap(r"C:\Users\Juliao JM\Downloads\py-white.ico")
 root.title("First Program")
 root.geometry("600x400")
 
+"""
+# That is show all messagebox
+# show info message boxes
+messagebox.showinfo("info","Hello, World!")
+# show warning message boxes
+messagebox.showwarning("warning","Hello, World!")
+# show error message boxes
+messagebox.showerror("error","Hello, ErrorX")
+"""
 
+# That is decision in messagebox
+# if( n:= messagebox.askquestion("question","do you want to continue...") ):
+# if( n:= messagebox.askyesno("yesno","do you want to continue...")):
+# ( n:= messagebox.askokcancel("okcancel","are you sure!")):
+if( n:= messagebox.askretrycancel("retrycancel","do yo want to stay the program!")):
+	print(f"user chose {n}")
+	messagebox.showinfo("info","the program will be continue...")
+else:
+	print(f"user chose {n}")
+	messagebox.showwarning("warning","the program will be close!")
+	root.destroy()
 
 root.mainloop()
