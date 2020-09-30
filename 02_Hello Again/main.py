ede5fb84aecc5ff1b9912325d77721b2ba426f5b
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-09-30 09:06:49 +0900

    Create method to include the all classes in tkinter inside the class

--- 02_Hello Again/main.py
+++ 02_Hello Again/main.py
@@ -6,6 +6,11 @@ class App(Frame):
 	def __init__(self,master=None):
 		super().__init__(master)
 		self.pack()
+		# call the method
+		self.all_here()
+
+	def all_here(self):
+		pass
 
 
 root = Tk()
