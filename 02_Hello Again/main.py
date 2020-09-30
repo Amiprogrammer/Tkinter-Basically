ee638fbbc921c887f1e131e94ab8321176ee40fb
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-09-30 09:28:36 +0900

    Add label class into program

--- 02_Hello Again/main.py
+++ 02_Hello Again/main.py
@@ -6,11 +6,17 @@ class App(Frame):
 	def __init__(self,master=None):
 		super().__init__(master)
 		self.pack()
+		self.master.geometry("800x600")
 		# call the method
 		self.all_here()
 
 	def all_here(self):
-		pass
+
+		self.status = Label(self)
+		self.status["text"] = "Hello, World!"
+		self.status["font"] = ("Courier New",28)
+		self.status.pack(pady=150)
+
 
 
 root = Tk()
