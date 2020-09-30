e3f953a08d43492d3cf3cccb421abced4b7da3dc
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-09-30 11:16:55 +0900

    Learn widget configuration in 03_Widget Configuration/main.py

--- 03_Widget Configuration/main.py
+++ 03_Widget Configuration/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from random import choice
 
 root = Tk()
 root.title("First Program")
@@ -7,10 +8,23 @@ root.geometry("400x200")
 x = Label(root, text="Widget Configuration")
 x.pack()
 
-x.config(pady=10)
-x.config(fg="blue")
-x.config(font=("Fira Code Medium",17))
-x.config(bg="black", fg="white")
+# x.config(pady=10)
+# x.config(fg="blue")
+# x.config(font=("Fira Code Medium",17))
+# x.config(bg="black", fg="white")
 x.config(pady=15)
+# x["bg"] = "white" or x.config(bg="white")
+x["fg"] = "black"
+x["font"] = ("Courier New",20)
+
+def here():
+	bg = choice(["red","yellow","purple","orange","green","pink","chocolate","lightblue"])
+	x.config(text=bg, bg=bg)
+	root.config(bg=bg)
+
+b = Button(root, text="change bg", command=here)
+b.pack()
+
+b.config(bg="black", fg="white", font=("Cooper Black",11))
 
 root.mainloop()
