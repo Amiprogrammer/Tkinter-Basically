a3929a95ce51ff4c96d05e17997e6f68d83e95b0
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-09-30 23:31:05 +0900

    Firs pulldown menus in tkinter learn 05_Application Windows\menus.py

--- 05_Applications Windows/menus.py
+++ 05_Applications Windows/menus.py
@@ -8,13 +8,40 @@ root.geometry("600x400")
 def func_here():
 	print("OK!")
 
-# pulldown menu
+"""
+# toplevel menu
 menubar = Menu(root)
 
 menubar.add_command(label="open", command=func_here)
 menubar.add_command(label="exit", command=root.destroy)
 
 # to display the menu
+root.config(menu=menubar)
+"""
+
+# this is pulldown menu
+menubar = Menu(root)
+
+filemenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="file", menu=filemenu)
+filemenu.add_command(label="open", command=func_here)
+filemenu.add_command(label="save", command=func_here)
+filemenu.add_command(label="save as", command=func_here)
+filemenu.add_separator()
+filemenu.add_command(label="exit", command=root.destroy)
+
+editmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="edit", menu=editmenu)
+editmenu.add_command(label="copy", command=func_here)
+editmenu.add_command(label="cut", command=func_here)
+editmenu.add_separator()
+editmenu.add_command(label="paste", command=func_here)
+
+helpmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="help", menu=helpmenu)
+helpmenu.add_command(label="about", command=func_here)
+
+# display menu
 root.config(menu=menubar)
 
 root.mainloop()
