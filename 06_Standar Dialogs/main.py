f152ab9aca5334312cf7b8321aa80675a3cd7776
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-01 00:35:40 +0900

    Now importing the messagebox module

---
+++ 06_Standar Dialogs/main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.iconbitmap(r"C:\Users\Juliao JM\Downloads\py-white.ico")
+root.title("First Program")
+root.geometry("600x400")
+
+
+
+root.mainloop()
