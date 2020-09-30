197af3c7c69a9240df47b449593b8d0711f7678a
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-09-30 23:15:48 +0900

    Learn how to set Toplevel classes in tkinter with base window

--- 05_Applications Windows/base.py
+++ 05_Applications Windows/base.py
@@ -3,7 +3,15 @@ from tkinter import *
 root = Tk()
 root.iconbitmap(r"C:\Users\Juliao JM\Downloads\py-white.ico")
 root.title("Firs Program")
+root.geometry("600x400")
 
+top = Toplevel()
+
+x1 = Message(top, text="this is program built by tkinter!")
+x1.pack()
+
+b = Button(top, text="DISMISS", bg="red", fg="white", command=top.destroy)
+b.pack()
 
 
 root.mainloop()
