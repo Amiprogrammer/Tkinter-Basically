7019405f40e70fb05137915276db6c6edf06cc8a
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-01 16:29:59 +0900

    testing connection to my database

--- 10_Entry Widget/A_Tkinter with MySQL/test.py
+++ 10_Entry Widget/A_Tkinter with MySQL/test.py
@@ -1,17 +1,11 @@
 from tkinter import *
 import mysql.connector
 
-db = mysql.connector.connect(
+mydb = mysql.connector.connect(
 	host="localhost",
 	username="root",
-	password="jmq123jj"
+	password="jmq123jj",
+	database="tkinter"
 )
 
-mycursor = db.cursor()
-
-# mycursor.execute("CREATE DATABASE tkinter")
-
-mycursor.execute("SHOW DATABASES")
-
-for x in mycursor:
-	print(x)
+print(mydb)
