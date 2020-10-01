3fd45c903a4a906d35b1d1c74938e455d9c2a880
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-01 13:23:59 +0900

    Create 07_Button Widget/main.py to learn how write button widget in tkinter

---
+++ 07_Button Widget/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+master = Tk()
+master.iconbitmap(r"C:/Users/Juliao JM/Downloads/py-white.ico")
+master.title("Button Widget")
+master.geometry("600x400")
+
+
+
+master.mainloop()
