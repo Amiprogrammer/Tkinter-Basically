db999694207f6ae1d0aabb1928b5305122fa1484
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-01 13:26:19 +0900

    Associate python function or method with button widget, when button is pressed automatically calls that function or method

--- 07_Button Widget/main.py
+++ 07_Button Widget/main.py
@@ -5,6 +5,10 @@ master.iconbitmap(r"C:/Users/Juliao JM/Downloads/py-white.ico")
 master.title("Button Widget")
 master.geometry("600x400")
 
+def func_here():
+	print("Button is pressed!")
 
+b1 = Button(master, text="OK", command=func_here)
+b1.pack()
 
 master.mainloop()
