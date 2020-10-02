5b38649b8a2bf7d445ba755a7fdcb5e8794e002c
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-02 15:32:32 +0900

    Learn LabelFrame in 13_LabelFrame Widget/main.py

--- 13_LabelFrame Widget/main.py
+++ 13_LabelFrame Widget/main.py
@@ -1,1 +1,14 @@
-main.py
+from tkinter import *
+
+master = Tk()
+master.iconbitmap(r"C:/Users/Juliao JM/Downloads/py-white.ico")
+master.title("LabelFrame Widget")
+master.geometry("600x400")
+
+name_index = LabelFrame(master,  text="Name", padx=10, pady=10)
+name_index.pack(padx=4, pady=4)
+
+name_entry = Entry(name_index)
+name_entry.pack()
+
+master.mainloop()
