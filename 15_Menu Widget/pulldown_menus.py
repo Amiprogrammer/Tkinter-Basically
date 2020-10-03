a83dbd9dbd0c7b6fc270a5665f9b4f76b61c2b27
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-03 12:45:24 +0900

    Learn pulldown menu is very cool in 15_Menu Widget/pulldown_menus.py

--- 15_Menu Widget/pulldown_menus.py
+++ 15_Menu Widget/pulldown_menus.py
@@ -19,6 +19,16 @@ filemenu.add_command(label="save as", command=ok)
 filemenu.add_separator() # to add separator
 filemenu.add_command(label="exit", command=master.destroy)
 
+editmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="edit", menu=editmenu)
+editmenu.add_command(label="copy", command=ok)
+editmenu.add_command(label="cut", command=ok)
+editmenu.add_command(label="paste", command=ok)
+
+helpmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="help", menu=helpmenu)
+helpmenu.add_command(label="about", command=ok)
+
 # to show the menu
 master.config(menu=menubar)
 
