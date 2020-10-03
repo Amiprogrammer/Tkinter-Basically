e078568ae64a02cc9504642b47560d6ee7ff4e12
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-03 12:41:54 +0900

    Learn pulldown menu how to set menu instance and separadot inside the menu instance

---
+++ 15_Menu Widget/pulldown_menus.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+master = Tk()
+master.iconbitmap(r"C:/Users/Juliao JM/Downloads/py-white.ico")
+master.title("Pulldown Menu")
+master.geometry("600x400")
+
+def ok():
+	print("OK!")
+
+menubar = Menu(master)
+
+# create menu instance
+filemenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="file", menu=filemenu) # to add menu instance
+filemenu.add_command(label="open", command=ok)
+filemenu.add_command(label="save", command=ok)
+filemenu.add_command(label="save as", command=ok)
+filemenu.add_separator() # to add separator
+filemenu.add_command(label="exit", command=master.destroy)
+
+# to show the menu
+master.config(menu=menubar)
+
+master.mainloop()
